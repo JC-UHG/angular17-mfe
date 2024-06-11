dddfe9b8b12ed2c266c29567e503f26aca829a3c
JC Lango <jobs@jclango.com>
2024-06-11 14:00:07 -0400

    Remove welcome component from xxx-mf-host.

---
+++ apps/xxx-mf-host/src/modules/xxx-home/xxx-home.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'xxx-home',
+  standalone: true,
+  imports: [CommonModule],
+  styles: [],
+  templateUrl: './xxx-home.component.html',
+  encapsulation: ViewEncapsulation.None,
+})
+export class XxxHomeComponent {}
